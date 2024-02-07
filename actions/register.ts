a2888072f4d07c2c4a2434a7813a6cf0aa8d7b71
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-02-07 12:49:30 +0100

    Implement register new user

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
 
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -11,7 +14,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: 'Invalid fields' };
   }
 
+  const { email, name, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'User already exists' };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send email with verification token
+
   return {
-    success: 'Successfully registered',
+    success: 'User successfully registered!',
   };
 };
