386780fd41657ceef03f74b8bb370de23d806250
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-02-19 15:33:48 +0100

    Add id and role to user.

---
+++ next-auth.d.ts
@@ -0,0 +1,17 @@
+import NextAuth, { type DefaultSession } from 'next-auth';
+
+export type ExtendedUser = DefaultSession['user'] & { role: 'ADMIN' | 'USER' };
+
+declare module 'next-auth' {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
+
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    role?: 'ADMIN' | 'USER';
+  }
+}
