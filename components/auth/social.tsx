6819294d30bf223725ce9fe1a0ea673323f7be41
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-02-20 12:29:31 +0100

    Implement Google and GitHub providers login

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,17 +1,35 @@
 'use client';
 
+import { signIn } from 'next-auth/react';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 
 import { Button } from '../ui/button';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 export const Social = () => {
+  const onClick = (provider: 'google' | 'github') => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="w-full flex items-center gap-x-2">
-      <Button variant="outline" className="w-full" size="lg" onClick={() => {}}>
+      <Button
+        variant="outline"
+        className="w-full"
+        size="lg"
+        onClick={() => onClick('google')}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button variant="outline" className="w-full" size="lg" onClick={() => {}}>
+      <Button
+        variant="outline"
+        className="w-full"
+        size="lg"
+        onClick={() => onClick('github')}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
