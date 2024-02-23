1000b38b70c860b5c8fe0d5bb7b7bbfb812059c3
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-02-23 13:34:27 +0100

    Implement Navbar with a menu and User button.

---
+++ components/auth/user-button.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { FaUser } from 'react-icons/fa';
+import { ExitIcon } from '@radix-ui/react-icons';
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { useCurrentUser } from '@/hooks/use-current-user';
+import { LogoutButton } from '@/components/auth/logout-button';
+
+const UserButton = () => {
+  const user = useCurrentUser();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={user?.image || ''} alt="Your profile picture" />
+          <AvatarFallback className="bg-sky-500">
+            <FaUser className="text-white" />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="end">
+        <LogoutButton>
+          <DropdownMenuItem>
+            <ExitIcon className="mr-2 h-4 w-4" />
+            Logout
+          </DropdownMenuItem>
+        </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserButton;
